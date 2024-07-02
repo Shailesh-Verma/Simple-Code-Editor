a716f3bb94b7125429a6f71f66e22531ecfac23a
Shailesh-Verma <shaileshverma238@gmail.com>
2024-07-02 14:49:36 +0530

    adding github link to button

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <div className="main-container">
       <h1 className="title">react-simple-code-editor</h1>
       <p className="sub-title">A simple no-frills code editor with syntax highlighting.</p>
-      <button className="btn-github">GitHub</button>
+      <button className="btn-github"><a href="https://github.com/Shailesh-Verma/Simple-Code-Editor.git" target="_blank">GitHub</a></button>
     </div>
     <CodeEditor />
     </>
